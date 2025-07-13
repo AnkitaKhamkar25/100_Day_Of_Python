443390b01b4c531a22ff082a1e60834539f346b5
AnkitaKhamkar25 <khamkarankita252@gmail.com>
2025-07-13 22:45:29 +0530

    Day six file added

---
+++ 06-Day6-Function/function.py
@@ -0,0 +1,11 @@
+#  What is a Function?
+# A function in Python is a reusable block of code that performs a specific task.
+# It helps to:
+    # Organize code into smaller, manageable pieces.
+    # Avoid code repetition.
+    # Make code reusable and readable.
+
+# priciple of function:
+    # abstraction
+    # decomposition
+
